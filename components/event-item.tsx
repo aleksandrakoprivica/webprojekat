358cf2ae910751cea1dd0c5bb82d10a620be0b6c
aleksandrakoprivica <alexandra.kop@gmail.com>
2023-05-07 12:31:24 +0200

    event item edit

--- components/event-item.tsx
+++ components/event-item.tsx
@@ -13,8 +13,31 @@ const EventItem: FC<EventItemProps> = ({ eventItem }) => {
         <div>name</div>
         <div>{eventItem.name}</div>
       </div>
-      <div>date</div>
-      <div>{eventItem.date}</div>
+        <div className={"flex flex-col items-center"}>
+            <div>date</div>
+            <div>{eventItem.date}</div>
+        </div>
+        <div className={"flex flex-col items-center"}>
+            <div>description</div>
+            <div>{eventItem.description}</div>
+        </div>
+        <div className={"flex flex-col items-center"}>
+            <div>location</div>
+            <div>{eventItem.location}</div>
+        </div>
+        <div className={"flex flex-col items-center"}>
+            <div>authorEmail</div>
+            <div>{eventItem.email}</div>
+        </div>
+        <div className={"flex flex-col items-center"}>
+            <div>type</div>
+            <div>{eventItem.type}</div>
+        </div>
+        <div className={"flex flex-col items-center"}>
+            <div>createdAt</div>
+            <div>{eventItem.createdAt}</div>
+        </div>
+
       <div>DELETE</div>
       <div>EDIT</div>
     </div>
--- components/form.tsx
+++ components/form.tsx
@@ -79,7 +79,7 @@ export default function Form({
           id="email"
           name="email"
           type="email"
-          placeholder="panic@thedis.co"
+          placeholder="example@gmail.com"
           autoComplete="email"
           required
           className="mt-1 block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-black focus:outline-none focus:ring-black sm:text-sm"
@@ -105,7 +105,7 @@ export default function Form({
         className={`${
           loading
             ? "cursor-not-allowed border-gray-200 bg-gray-100"
-            : "border-black bg-black text-white hover:bg-white hover:text-black"
+            : "border-black bg-cyan-600 text-white hover:bg-white hover:text-black"
         } flex h-10 w-full items-center justify-center rounded-md border text-sm transition-all focus:outline-none`}
       >
         {loading ? (
